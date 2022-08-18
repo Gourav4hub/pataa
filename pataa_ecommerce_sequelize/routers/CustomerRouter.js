507ff3c8c918726d89da2d3fdb191b34ef06837e
codebetter-gourav <you@example.com>
2022-08-18 19:46:52 +0530

    18aug

--- pataa_ecommerce_sequelize/routers/CustomerRouter.js
+++ pataa_ecommerce_sequelize/routers/CustomerRouter.js
@@ -1,12 +1,65 @@
 const express = require('express')
 const ApiResponse = require('./responses/ApiResponse')
-const {Customer} = require('../models/index')
+const {Customer,Cart,Product,Order,OrderItem,sequelize} = require('../models/index')
 
 const router = express.Router()
 
-router.post("/placeorder",(req,res)=>
+router.get("/orders",async (req,res)=>
 {
-    
+    var email = req.user_email;
+    var customer = await Customer.findOne({where:{email}})
+    var orders = await Order.findAll({
+        where:{customer:customer.id},
+        include : 'orderitems'
+    })
+    res.status(200).json(new ApiResponse(true,orders,"Customer Orders !"))
+})
+
+router.post("/placeorder",async (req,res)=>
+{
+    var email = req.user_email;
+    var customer = await Customer.findOne({where:{email}})
+    if(customer!=null)
+    {
+        const transaction = await sequelize.transaction();
+        try {
+        var carts = await Cart.findAll({
+            where:{customer:customer.id},
+            include : 'prod'
+        })
+        var totalAmount = carts.reduce((preValue,currentObj)=>
+        {
+            var ob = currentObj.dataValues
+            var amt = (ob.qty * ob.prod.dataValues.product_price)-ob.discount
+            return amt + preValue
+        },0)
+        var order = await Order.create({total_amount:totalAmount,customer:customer.id},{ transaction})
+
+        var items = carts.map(cart=>{
+            var ob = {
+                qty : cart.qty , 
+                price : cart.prod.dataValues.product_price,
+                discount : cart.discount,
+                amount : (cart.qty * cart.prod.dataValues.product_price)-cart.discount,
+                product : cart.prod.dataValues.id,
+                order : order.dataValues.id
+            }
+            return ob
+        });
+        
+        var item = await OrderItem.bulkCreate(items,{ transaction})
+
+        await Cart.destroy({ where: { customer : customer.id }},{transaction})
+        await transaction.commit()
+        res.status(200).json(new ApiResponse(true,order,"Customer Order Placed !"))
+    }catch(err){
+        await transaction.rollback()
+        res.status(500).json(new ApiResponse(false,null,"Somthing Wrong !"))
+    }
+       
+    }else{
+        res.status(400).json(new ApiResponse(false,null,"Customer Not Found !"))
+    }
 })
 
 router.get("/myinfo",async (req,res)=>
@@ -23,3 +76,19 @@ router.get("/myinfo",async (req,res)=>
 
 
 module.exports = router;
+
+/*
+for(var cart of carts)
+        {
+            var ob = {
+                qty : cart.qty , 
+                price : cart.prod.dataValues.product_price,
+                discount : cart.discount,
+                amount : (cart.qty * cart.prod.dataValues.product_price)-cart.discount,
+                product : cart.prod.dataValues.id,
+                order : order.dataValues.id
+            }
+            var item = await OrderItem.create(ob)
+            res.status(200).json(new ApiResponse(true,order,"Customer Order Placed !"))
+        }
+*/
